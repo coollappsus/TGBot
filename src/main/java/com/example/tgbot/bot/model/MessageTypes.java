ae531ffd57121a15e4a321548639b60e2cee7e1c
Ivan Ryzhikov <107796289+coollappsus@users.noreply.github.com>
2024-12-19 15:07:09 +0300

    -Изменение цен

--- src/main/java/com/example/tgbot/bot/model/MessageTypes.java
+++ src/main/java/com/example/tgbot/bot/model/MessageTypes.java
@@ -1,9 +1,12 @@
 package com.example.tgbot.bot.model;
 
+import lombok.Getter;
+
+@Getter
 public enum MessageTypes {
-    AUDIO((1.75 + 0.45) / 60, 0.177 / 1000),
-    AUDIO_TRANSCRIBE(1.75 / 60, 0.1 / 1000),
-    TEXT(0.045 / 1000, 0.177 / 1000);
+    AUDIO((1.8 + 0.45) / 60, 0.177 / 1000),
+    AUDIO_TRANSCRIBE(1.8 / 60, 0.1 / 1000),
+    TEXT(0.05 / 1000, 0.177 / 1000);
 
     private final double requestPrice;
     private final double responsePrice;
@@ -11,13 +14,5 @@ public enum MessageTypes {
     MessageTypes(double requestPrice, double responsePrice) {
         this.requestPrice = requestPrice;
         this.responsePrice = responsePrice;
-    }
-
-    public double getRequestPrice() {
-        return requestPrice;
-    }
-
-    public double getResponsePrice() {
-        return responsePrice;
     }
 }
