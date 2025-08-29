da970e45000ab85ca859f94bfef1804f407ffc55
Ivan Ryzhikov <107796289+coollappsus@users.noreply.github.com>
2025-08-29 18:12:55 +0300

    Изменение ИБ

---
+++ application.tpl
@@ -0,0 +1,8 @@
+spring.application.name=TGBot
+token.bot=${BOT_TOKEN}
+open.ai.token=${OPEN_AI_TOKEN}
+
+spring.datasource.url=${DB_URL}
+spring.datasource.username=${DB_USERNAME}
+spring.datasource.password=${DB_PASSWORD}
+spring.jpa.generate-ddl=true
--- src/main/java/com/example/tgbot/bot/TgBotApplication.java
+++ src/main/java/com/example/tgbot/bot/TgBotApplication.java
@@ -1,11 +1,9 @@
 package com.example.tgbot.bot;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEncryptableProperties
 public class TgBotApplication {
 
     public static void main(String[] args) {
